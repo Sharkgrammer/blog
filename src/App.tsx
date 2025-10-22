0773231c545550eb15372c5b1b6383e7bd067ac7
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2025-10-22 22:04:01 +0100

    Forgot min width

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Outlet} from "react-router-dom";
 function App() {
 
     return (
-        <div className="min-h-screen h-screen mx-auto max-w-[500px]">
+        <div className="min-h-screen h-screen mx-auto w-5/6 max-w-[500px]">
             <Outlet/>
         </div>
     )
